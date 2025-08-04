3c640458364878fe9b9b9c27dd29deb657e455d7
ZaenbA <honeysorataiga@gmail.com>
2025-08-04 15:52:29 +0100

    add filter

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,5 @@ from .models import Post
 
 
 class PostList(generic.ListView):
-    queryset = Post.objects.filter(status=1).order_by('-created_on')
-    template_name = 'blog/post_list.html'
-    paginate_by = 6
+       queryset = Post.objects.filter(status=1)
+    template_name = "post_list.html"
